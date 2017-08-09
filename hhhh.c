9738d968fb329561238dd331b95d80b4cc2e7264
Gowtham <30343102+gowtham02@users.noreply.github.com>
2017-08-09 15:47:05 +0530

    Update hhhh.c

--- hhhh.c
+++ hhhh.c
@@ -6,9 +6,9 @@ int num;
 printf("enter the number\n");
 scanf("%d",&num);
 if (num<0)
-printf("num is positive");
-else if(num>0)
 printf("num is negative");
+else if(num>0)
+printf("num is positive");
 else
 printf("num is zero");
 return 0;
